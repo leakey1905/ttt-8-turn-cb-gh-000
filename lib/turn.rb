ea8dc8ae85b8e9c6b68bc8dc351f73f89a20d977
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-20 08:46:55 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,12 +7,23 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+# move the things
 def move(board, index, value = "X")
   board[index] = value
   return board
 end
+
+def convert_input_to_index(input)
+  index = input.to_i
+  return index
+end
+
 # its my turn
 def turn(board)
   puts "Please enter 1-9:"
+  input = get.strip
+  index = convert_input_to_index(input)
+  if index.between?(0, 8)
+    move(board, index, value)
 
 end
