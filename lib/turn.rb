968d42e60254f11da2df543bf1b9a3d8160f079f
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-20 08:38:22 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,7 +7,11 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def move(board, index, value = "X")
+
+end
 # its my turn
 def turn(board)
   puts "Please enter 1-9:"
+  
 end
